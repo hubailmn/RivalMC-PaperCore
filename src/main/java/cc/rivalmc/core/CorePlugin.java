1cdb0864baa2f0ac08f61c729d7b6c2a96854e16
hubailmn <hubailmn@gmail.com>
2025-07-12 14:50:56 +0300

    init

---
+++ src/main/java/cc/rivalmc/core/CorePlugin.java
@@ -0,0 +1,16 @@
+package cc.rivalmc.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CorePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
